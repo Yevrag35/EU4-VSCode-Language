3728f153c772bf3c00bfd2c9e82c5c55dea58172
Mike Garvey <mike.garvey@yevrag35.com>
2021-05-10 18:19:53 -0400

    easier way

---
+++ .build/build.ps1
@@ -0,0 +1,13 @@
+﻿$curDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$eu4Code = "$curDir\..\eu4-code"
+[string[]] $highLevelScopes = Get-Content -Path "$eu4Code\high-level-scopes.txt" | Sort-Object
+$joinAll = $highLevelScopes -join '|'
+
+$syntaxFile = Get-ChildItem -Path "$eu4Code\syntaxes" -Filter *.tmLanguage.json
+$syntaxContent = $syntaxFile | Get-Content -Raw
+
+$replaceWith = $syntaxContent -creplace 'BOOM[_]REPLACE[_]ME[_]BOOM', $joinAll
+Set-Content -Path $syntaxFile.FullName -Value $replaceWith -Force
+
+Copy-Item -Path $eu4Code "$env:USERPROFILE\.vscode\extensions" -Recurse -Force
