3106dfaba23e637311e291c4f6f07334905372a4
Mike Garvey <mike.garvey@yevrag35.com>
2021-05-16 13:57:46 -0400

    Maybe

---
+++ eu4-ls/BufferManager.cs
@@ -0,0 +1,20 @@
+using Microsoft.Language.Xml;
+using System.Collections.Concurrent;
+
+namespace EUIVServer
+{
+    internal class BufferManager
+    {
+        private ConcurrentDictionary<string, Buffer> _buffers = new ConcurrentDictionary<string, Buffer>();
+
+        public void UpdateBuffer(string documentPath, Buffer buffer)
+        {
+            _buffers.AddOrUpdate(documentPath, buffer, (k, v) => buffer);
+        }
+
+        public Buffer GetBuffer(string documentPath)
+        {
+            return _buffers.TryGetValue(documentPath, out var buffer) ? buffer : null;
+        }
+    }
+}
---
+++ eu4-ls/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace eu4_ls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
